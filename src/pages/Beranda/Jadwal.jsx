59bd4beb0c221f608f3a539500debfa796277a36
miftaaaaaa <miftakhul.20015@mhs.unesa.ac.id>
2023-07-01 02:30:11 +0700

    update: responsive beranda

--- src/pages/Beranda/Jadwal.jsx
+++ src/pages/Beranda/Jadwal.jsx
@@ -125,13 +125,13 @@ const JadwalPenerbangan = () => {
 
     return (
         <>
-            <Card className="relative mx-auto md:w-1/2 lg:w-2/3 xl:w-2/3 lg:-mt-10 xl:-mt-14">
+            <Card className="relative flex flex-row mx-auto md:w-1/2 lg:w-2/3 xl:w-2/3 lg:-mt-10 xl:-mt-14">
                 <p className="text-lg font-bold lg:ml-4 xl:ml-4">Pilih Jadwal Penerbangan spesial di<span className=" text-primary2"> TerbangIn!</span></p>
-                <div className="grid grid-cols-3 mt-3">
+                <div className="grid md:grid-cols-3 grid-cols mt-5">
                     <div>
                         <div className="flex items-center lg:ml-4 xl:ml-4">
                             <Image src={icon_pesawat} alt="icon_pesawat" className="lg:w-4 xl:w-4" />
-                            <p className="text-primary1 text-xs md:text-base ml-2 mr-2 lg:ml-2 xl:ml-2">From</p>
+                            <p className="text-primary1 text-base md:text-base ml-2 mr-2 lg:ml-2 xl:ml-2">From</p>
                             <div >
                                 <ModalFlightFrom
                                     value={from}
@@ -141,32 +141,32 @@ const JadwalPenerbangan = () => {
                                 />
                             </div>
                         </div>
-                        <hr className="flex border-1 lg:w-52 xl:w-60 lg:ml-[102px] xl:ml-[100px]" />
-                        <div className="">
-                            <div className="flex items-center mx-10 mt-7">
+                        <hr className="flex md:ml-0 ml-[70px] border-1 lg:w-52 xl:w-60 w-44 lg:ml-[102px] xl:ml-[100px]" />
+                        <div className="flex flex-col">
+                            <div className="flex items-center mx-10 mt-7 gap-5">
                                 <div className="text-base text-primary1 mx-10 lg:ml-16 xl:ml-14">Departure</div>
-                                <div className="text-base text-primary1 lg:ml-6 lg:mr-12 xl:ml-20 xl:mr-16">Return</div>
-                                <div><InputSwitch checked={checked} onChange={(e) => setChecked(e.value)} /></div>
+                                <div className="text-base text-primary1 lg:ml-6 lg:mr-12 xl:ml-20  xl:mr-16">Return</div>
+                                <div className="md:ml-0 ml-5"><InputSwitch checked={checked} onChange={(e) => setChecked(e.value)} /></div>
                             </div>
-                            <div className="flex items-center lg:mt-2 -mr-28 lg:ml-4 xl:ml-4 max-[1058px]:bg-sky-300">
+                            <div className="flex items-center lg:mt-2 -mr-28 lg:ml-4 xl:ml-4 md:pb-0 md:gap-0 gap-2 pb-11">
                                 <Image src={icon_date} alt="icon_date" className="w-4 md:w-6 xl:ml-1" />
                                 <div className="text-base text-primary1 lg:ml-2 xl:px-1">Date</div>
-                                <Calendar value={selectedDate1} onChange={handleDate1Change} numberOfMonths={2} dateFormat="dd MM yy" className="min-[1024px]:w-70 max-[1058px]: min-[1154px]:w-60 max-[1279px]:w-60 w-56 h-7 lg:mx-8 xl:ml-3 " />
-                                <Calendar value={selectedDate2} onChange={handleDate2Change} numberOfMonths={2} disabled={!checked} minDate={selectedDate1} dateFormat="dd MM yy" className="min-[1024px]: max-[1058px]:-ml min-[1154px]:w-60 min-[1154px]:-ml-5 max-[1279px]:-ml-5 xl:w-60 h-7 xl:ml-3 xl:mr-3" />
+                                <Calendar value={selectedDate1} onChange={handleDate1Change} numberOfMonths={2} dateFormat="dd MM yy" className="md:w-60 w-28  h-7 lg:mx-8 xl:ml-3 ml-3 mr-27 border-b outline-none" />
+                                <Calendar value={selectedDate2} onChange={handleDate2Change} numberOfMonths={2} disabled={!checked} minDate={selectedDate1} dateFormat="dd MM yy" className="md:w-60 w-28 -ml-24 xl:w-60 h-7 xl:ml-3 xl:mr-3 border-b" />
                             </div>
-                            <div className="pt-1">
-                                <hr className="flex border-1 min-[1024px]:ml-[103px] max-[1058px]:ml-[103px] min-[1154px]:w-32 max-[1279px]:w-32 lg:w-28 xl:ml-24 xl:w-[153px]" />
-                                <hr className="border-1 min-[1024px]:ml-60 min-[1154px]:w-32 max-[1279px]:w-32 max-[1058px]:ml-60 lg:w-[110px] xl:ml-72 xl:w-[153px]" />
-                            </div>
+                            {/* <div className="pt-1"> */}
+                                {/* <hr className="flex border-1 md:w-95 w-16 md:ml-100 ml-10" />
+                                <hr className="border-1 min-[1024px]:ml-60 min-[1154px]:w-32 max-[1279px]:w-32 max-[1058px]:ml-60 lg:w-[110px] xl:ml-72 xl:w-[153px]" /> */}
+                            {/* </div> */}
 
                         </div>
                     </div>
                     <div className="mx-auto"><Image src={return1} alt="return1" onClick={return1Handler} className="cursor-pointer xl:w-4" /></div>
                     <div className="-ml-24 -mr-4">
-                        <div className="flex items-center">
-                            <Image src={icon_pesawat} alt="icon_pesawat" className="w-6 mr-2 ml-1 sm:w-4 min-[1154px]:ml-2 max-[1279px]:2 lg:w-4 lg:mr-3 lg:ml-4 xl:mr-3" />
-                            <p className="text-primary1 text-xs md:text-base mr-2">To</p>
-                            <div>
+                        <div className="flex items-center md:mt-0 mt-10">
+                            <Image src={icon_pesawat} alt="icon_pesawat" className="w-6 mr-2 ml-24 sm:w-4 min-[1154px]:ml-2 max-[1279px]:2 lg:w-4 lg:mr-3 lg:ml-4 xl:mr-3" />
+                            <p className="text-primary1 text-base md:text-base mr-2 lg:ml-2 xl:ml-2">To</p>
+                            <div className="">
                                 <ModalFlightTo
                                     value={to}
                                     onChange={handleToChange}
@@ -176,20 +176,21 @@ const JadwalPenerbangan = () => {
                                 />
                             </div>
                         </div>
-                        <hr className="flex border-1 lg:w-60 lg:ml-20" />
-                        <div className="mx-4 -mr-10">
+                        {/* <hr className="flex md:ml-0 ml-[70px] border-1 lg:w-52 xl:w-60 w-44 lg:ml-[102px] xl:ml-[100px]" /> */}
+                        <hr className="flex border-1 ml-38 lg:w-60 lg:ml-20 xl:w-60 w-44" />
+                        <div className="mx-4 -mr-10 md:ml-5 ml-25">
                             <div className="flex mx-6 mt-8">
                                 <div className="text-base text-primary1 min-[1154px]:ml-7 max-[1279px]:ml-7 xl:ml-11">Passengers</div>
-                                <div className="text-base text-primary1 min-[1154px]:ml-16 max-[1279px]:ml-16 xl:ml-14">Seat Class</div>
+                                <div className="text-base text-primary1 md:mx-18 mx-20">Seat Class</div>
                             </div>
-                            <div className="flex items-center mt-4">
-                                <Image src={airline_seat} alt="airline_seat" className="w-4 sm:w-5 md:w-6 min-[1154px]:-ml-2 max-[1279px]:-ml-2 xl:mr-2" />
+                            <div className="flex items-center md:mt-4 mt-1">
+                                <Image src={airline_seat} alt="airline_seat" className="w-7 md:w-6 min-[1154px]:-ml-2 max-[1279px]:-ml-2 xl:mr-2" />
                                 <div className="text-base text-primary1">To</div>
-                                <ModalPassengers value={passenger} />
+                                <ModalPassengers className="border-b" value={passenger} />
                                 <ModalSeatClass value={seatClass} onChange={handleSeatClassChange} />
                             </div>
-                            <hr className="flex border-1 min-[1154px]:w-28 max-[1279px]:w-28 min-[1154px]:ml-[54px] max-[1279px]:ml-[54px] xl:ml-[67px] xl:w-28" />
-                            <hr className="border-1 min-[1154px]:w-36 max-[1279px]:w-36 min-[1154px]:ml-[190px] max-[1279px]:ml-[190px] xl:ml-[205px] xl:w-34 mb-5" />
+                            <hr className="flex border-1 w-32 min-[1154px]:ml-[54px] max-[1279px]:ml-[54px] xl:ml-[67px] xl:w-28" />
+                            <hr className="border-1 md:ml-56 ml-52 md:w-40 w-24 mb-5" />
                         </div>
                     </div>
                 </div>
