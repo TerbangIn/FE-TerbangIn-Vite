6b0198644110443fe867788c3f053cd3e8e531e3
OryzaAZ <zoryzaamaliatuz@gmail.com>
2023-07-03 19:51:30 +0700

    update responsive banner and jadwal

--- src/pages/Beranda/Banner.jsx
+++ src/pages/Beranda/Banner.jsx
@@ -5,9 +5,9 @@ import banner4 from "../../assets/images/img_banner.svg"
 function Banner() {
     return (
         <>
-            <div className="bg-gradient-to-r from-purple4 to-purple2 w-full h-10 mt-6 sm:h-20 md:h-20 lg:h-28 sm:mt-6 md:mt-6 lg:mt-12 xl:mt-16"></div>
-            <div className="flex justify-center -mt-14 sm:-mt-24 md:-mt-30 lg:-mt-36">
-                <Image src={banner4} alt="img_banner" className=" pl-4 flex items-center pr-4 sm:w-11/12 md:w-11/12 lg:w-11/12 xl:w-[1250px]" />
+           <div className="bg-gradient-to-r from-purple4 to-purple2 w-full h-10 mt-6 sm:h-20 md:h-20 lg:h-28 sm:mt-6 md:mt-6 lg:mt-14 xl:mt-16"></div>
+            <div className="flex justify-center -mt-14 sm:-mt-24 md:-mt-30 lg:-mt-36 2xl:">
+                <Image src={banner4} alt="img_banner" className="pl-4 flex items-center pr-4 sm:w-11/12 md:w-11/12 lg:w-[1100px] xl:w-[1100px] 2xl:w-[1600px] 2xl:mx-48" />
             </div>
         </>
 
